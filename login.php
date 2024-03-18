b80159d00030f5261e6fa6d2397ccd1f354854c8
crazyycoder <crazyycoder805@gmail.com>
2024-03-18 20:39:33 +0500

    loign.php mai aik input field rakhi hai 2 k bajaye working bhi hor rahi hai

--- login.php
+++ login.php
@@ -9,42 +9,37 @@ require_once 'assets/includes/head.php';
 
 if (isset($_POST['username'])) { 
     if (!empty($_POST['password']) && !empty($_POST['username'])) {
-        $user = $pdo->read('access', ['username'=>$_POST['username'], 'password'=>$_POST['password']]);
-    
+        if (filter_var($_POST['username'], FILTER_VALIDATE_EMAIL)) {
+            if ($pdo->validateInput($_POST['username'], 'email')) {
+                $user = $pdo->read('access', ['email'=>$_POST['username'], 'password'=>$_POST['password']]);
         
-        if (!empty($user)) {
-            $_SESSION['guardmark_user_id'] = $user[0]['id'];
-
-            $_SESSION['guardmark_username'] = $user[0]['username'];
-            $_SESSION['guardmark_email'] = $user[0]['username'];
-
-            $_SESSION['guardmark_role_id'] = $user[0]['role_id'];
-            $_SESSION['guardmark_email'] = $user[0]['email'];
-            $_SESSION['guardmark_user_id'] = $user[0]['user_id'];
-
             
-            if ($_SESSION['guardmark_role_id'] == 1) {
-                header('location:dashboard.php');
-            } else if ($_SESSION['guardmark_role_id'] == 3) {
-                header('location:guard_duties.php');
+                if (!empty($user)) {
+                    $_SESSION['guardmark_user_id'] = $user[0]['id'];
+        
+                    $_SESSION['guardmark_username'] = $user[0]['username'];
+                    $_SESSION['guardmark_email'] = $user[0]['username'];
+        
+                    $_SESSION['guardmark_role_id'] = $user[0]['role_id'];
+                    $_SESSION['guardmark_email'] = $user[0]['email'];
+                    $_SESSION['guardmark_user_id'] = $user[0]['user_id'];
+
+                    
+                    if ($_SESSION['guardmark_role_id'] == 1) {
+                        header('location:site_registration.php');
+                    } else if ($_SESSION['guardmark_role_id'] == 3) {
+                        header('location:guard_duties.php');
+                    }
+                } else {
+                    $error = "Username or password is not correct.";
+                }
+            } else {
+                $error = "Invalid email type.";
+ 
             }
         } else {
-            $error = "User does'nt exsit";
-        }
-        
-    } else {
-        $error = "(Username Or Email) & Password must be filled correctly.";
-    }
-    
-}
+            $user = $pdo->read('access', ['username'=>$_POST['username'], 'password'=>$_POST['password']]);
 
-if (isset($_POST['email'])) { 
-    if (!empty($_POST['password']) &&  !empty($_POST['email'])) {
-        if ($pdo->validateInput($_POST['email'], 'email')) {
-
-            $user = $pdo->read('access', ['email'=>$_POST['email'], 'password'=>$_POST['password']]);
-        
-            
             if (!empty($user)) {
                 $_SESSION['guardmark_user_id'] = $user[0]['id'];
     
@@ -54,25 +49,27 @@ if (isset($_POST['email'])) {
                 $_SESSION['guardmark_role_id'] = $user[0]['role_id'];
                 $_SESSION['guardmark_email'] = $user[0]['email'];
                 $_SESSION['guardmark_user_id'] = $user[0]['user_id'];
-
+    
                 
                 if ($_SESSION['guardmark_role_id'] == 1) {
-                    header('location:site_registration.php');
+                    header('location:dashboard.php');
                 } else if ($_SESSION['guardmark_role_id'] == 3) {
                     header('location:guard_duties.php');
                 }
             } else {
-                $error = "User does'nt exsit";
+                $error = "Username or password is not correct.";
             }
-        } else {
-            $error = "Invalid email format.";
-    
         }
+
+        
+        
     } else {
         $error = "(Username Or Email) & Password must be filled correctly.";
     }
     
 }
+
+
 ?>
 
 
@@ -121,7 +118,7 @@ if (isset($_POST['email'])) {
                             <p>Please Enter Your Details Below to Continue</p>
                             <div class="ad-auth-form">
                                 <div class="ad-auth-feilds">
-                                    <input type="text" placeholder="Username" name="username" id="username"
+                                    <input type="text" placeholder="Username / Email" name="username" id="username"
                                         class="ad-input" />
                                     <div class="ad-auth-icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 483.3 483.3">
@@ -136,22 +133,7 @@ if (isset($_POST['email'])) {
                                         </svg>
                                     </div>
                                 </div>
-                                ----OR----
-                                <div class="ad-auth-feilds mb-30">
-                                    <input type="email" placeholder="Email" name="email" id="email" class="ad-input" />
-                                    <div class="ad-auth-icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 483.3 483.3">
-                                            <path
-                                                d="M424.3,57.75H59.1c-32.6,0-59.1,26.5-59.1,59.1v249.6c0,32.6,26.5,59.1,59.1,59.1h365.1c32.6,0,59.1-26.5,59.1-59.1    v-249.5C483.4,84.35,456.9,57.75,424.3,57.75z M456.4,366.45c0,17.7-14.4,32.1-32.1,32.1H59.1c-17.7,0-32.1-14.4-32.1-32.1v-249.5    c0-17.7,14.4-32.1,32.1-32.1h365.1c17.7,0,32.1,14.4,32.1,32.1v249.5H456.4z"
-                                                data-original="#000000" class="active-path" data-old_color="#000000"
-                                                fill="#9abeed"></path>
-                                            <path
-                                                d="M304.8,238.55l118.2-106c5.5-5,6-13.5,1-19.1c-5-5.5-13.5-6-19.1-1l-163,146.3l-31.8-28.4c-0.1-0.1-0.2-0.2-0.2-0.3    c-0.7-0.7-1.4-1.3-2.2-1.9L78.3,112.35c-5.6-5-14.1-4.5-19.1,1.1c-5,5.6-4.5,14.1,1.1,19.1l119.6,106.9L60.8,350.95    c-5.4,5.1-5.7,13.6-0.6,19.1c2.7,2.8,6.3,4.3,9.9,4.3c3.3,0,6.6-1.2,9.2-3.6l120.9-113.1l32.8,29.3c2.6,2.3,5.8,3.4,9,3.4    c3.2,0,6.5-1.2,9-3.5l33.7-30.2l120.2,114.2c2.6,2.5,6,3.7,9.3,3.7c3.6,0,7.1-1.4,9.8-4.2c5.1-5.4,4.9-14-0.5-19.1L304.8,238.55z"
-                                                data-original="#000000" class="active-path" data-old_color="#000000"
-                                                fill="#9abeed"></path>
-                                        </svg>
-                                    </div>
-                                </div>
+
                                 <div class="ad-auth-feilds">
                                     <input type="password" placeholder="Password" name="password" id="password"
                                         class="ad-input" />
